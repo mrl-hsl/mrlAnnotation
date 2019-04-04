10eec91d5f5c056c436bdcad4d40ac1f21d93a4e
mhdelavaran <mhdelavaran@gmail.com>
2019-04-04 12:30:22 +0430

    revision selector

--- src/selector.h
+++ src/selector.h
@@ -3,16 +3,39 @@
 
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-
+#include "sample.h"
+struct Polygon{
+    Polygon() {}
+    std::vector<cv::Point> points;
+    typedef std::vector<cv::Point>::iterator iterator;
+    iterator begin(){return points.begin();}
+    iterator end(){return points.end();}
+};
+struct Line{
+    Line() {}
+    cv::Point p1,p2;
+};
 class Selector{
 public:
     Selector (){}
     ~Selector(){}
-    void is_drawing(bool);
+    void setStatus(bool);
     bool is_drawing();
+
+    void selectBox(cv::Point,int);
+    void selectPolygon(cv::Point,int);
+    void selectLine(cv::Point,int);
+    void selectSegment(cv::Scalar,int);
+
 private:
     bool drawing = false;
     std::vector<cv::Point2d> pts;
+    std::vector<bbox> objects;
+    std::vector<Polygon> polygons;
+    std::vector<Line> lines;
+    cv::Mat mask;
+
+
 };
 
 #endif // SELECTORS_H
